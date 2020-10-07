db7763aa653bfd1b9d2d72f3eeeb9d34c2e49fc2
16Hongc <59992618+16Hongc@users.noreply.github.com>
2020-10-07 13:09:44 +0900

    수정

--- Java/Lecture/16.클래스와 객체 II-4/src/lecture_16/Company.java
+++ Java/Lecture/16.클래스와 객체 II-4/src/lecture_16/Company.java
@@ -8,11 +8,11 @@ public class Company {
 	// 디폴트 생성자 정의. 외부에서 생성자를 호출하지 못하도록 private 지정
 	
 	public static Company getInstance() {     
-	// 외부에서 instance를 가져다 쓰기위한 메소드. 객체를 생성하지 않고 부르기 위해 static 지정
+	// 외부에서 instance를 가져다 쓰기위한 메소드. 
+	// 객체를 생성하지 않고 부르기 위해 static 지정
 		if(instance == null) ;
 			instance = new Company() ;
 		return instance ;
 	}
 
 }
-
